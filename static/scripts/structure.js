ca9072911e76985332894a3493e263cf76426812
QuantumBrainDisorder <74936907+QuantumBrainDisorder@users.noreply.github.com>
2021-08-23 23:18:23 +0200

    12

--- static/scripts/structure.js
+++ static/scripts/structure.js
@@ -1,5 +1,5 @@
 function structure__set__A() {
-  document.getElementById("structure__unit").style.backgroundColor = "green";
+  document.getElementById("structure__unit").textContent = 'Alpha<ul contenteditable="false"><li onclick="structure__set__A()">&#8491;</li><li onclick="structure__set__nm()">nm</li><li onclick="structure__set__um()">&#956;m</li></ul>';
 }
 
 function structure__set__nm() {
@@ -7,6 +7,5 @@ function structure__set__nm() {
 }
   
 function structure__set__um() {
-    document.getElementById("structure__unit").style.color = "red";
+    document.getElementById("structure__unit").style.color = "green";
  }
-  
