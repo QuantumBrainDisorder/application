7dce9a76544ab52d034132227d4e370429c312e3
QuantumBrainDisorder <74936907+QuantumBrainDisorder@users.noreply.github.com>
2021-08-23 22:45:38 +0200

    12

--- static/scripts/structure.js
+++ static/scripts/structure.js
@@ -1,3 +1,12 @@
-function structure__unit() {
-  document.getElementById("structure__unit").style.color = "red";
+function structure__set__A() {
+  document.getElementById("structure__unit").style.backgroundColor = "green";
 }
+
+function structure__set__nm() {
+    document.getElementById("structure__unit").style.color = "red";
+}
+  
+function structure__set__um() {
+    document.getElementById("structure__unit").style.color = "red";
+ }
+  
