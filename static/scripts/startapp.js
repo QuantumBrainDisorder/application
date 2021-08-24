c25328f6bed293c1cfc144d9007a2f53e98019eb
QuantumBrainDisorder <74936907+QuantumBrainDisorder@users.noreply.github.com>
2021-08-24 17:08:48 +0200

    21

--- static/scripts/startapp.js
+++ static/scripts/startapp.js
@@ -1,1 +1,13 @@
-structure__unit.innerHTML = localStorage.getItem("structure__unit")
+if (localStorage.getItem("structure__unit")){
+    structure__unit.innerHTML = localStorage.getItem("structure__unit");
+}
+else {
+    structure__unit.innerHTML = "nm";
+}
+
+if (localStorage.getItem("structure")){
+    structure.innerHTML = localStorage.getItem("structure");
+}
+else {
+    structure.value= "GaAs";
+}
--- static/scripts/structure.js
+++ static/scripts/structure.js
@@ -1,4 +1,6 @@
 const structure__unit = document.getElementById("structure__unit");
+const structure = document.getElementById("structure");
+
 
 function set__structure__unit(unit) {
   switch(unit) {
@@ -19,10 +21,23 @@ function set__structure__unit(unit) {
 
 structure__unit.onkeypress = function (event) {
     if (event.keyCode === 13) {
+      if (structure__unit.textContent == "") {
+        structure__unit.textContent = "nm";
+      }
       document.getElementById("structure__unit").blur();
     }
 }
 
 structure__unit.oninput = function (event) {
   localStorage.setItem("structure__unit", structure__unit.textContent);
+}
+
+structure__unit. = function (event) {
+  if (structure__unit.textContent == "") {
+    structure__unit.textContent = "nm";
+  }
+}
+
+structure.oninput = function (event) {
+  localStorage.setItem("structure", structure.value);
 }
